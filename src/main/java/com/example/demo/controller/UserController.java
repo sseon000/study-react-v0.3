d529060808e0df100d28bd11862b96a0187ddc1e
bugNick <rlatjsgur000@naver.com>
2023-01-04 23:33:16 +0900

    UserDTO & UserController(signup)

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            // 요청을 이용해 저장할 사용자 만들기
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+            // 서비스를 이용해 리포지터리에 사용자 저장
+            UserEntity registerUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registerUser.getEmail())
+                    .id(registerUser.getId())
+                    .username(registerUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            // 사용자 정보는 항상 하나이므로 리스트로 만들어야 하는 ResponseDTO를 사용하지 않고 그냥 UserDTO 리턴
+            // 근데 왜 ResponseDTO 리턴함?? 알아보자 2023.01.04
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
